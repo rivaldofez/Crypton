395437eff3baa33e34d53bf22e7373e9963f3552
rivaldofez <rivaldofez@gmail.com>
2020-09-24 22:43:06 +0700

    fix asset bug

--- app/src/main/java/com/example/crypton/SplashSccreen.java
+++ app/src/main/java/com/example/crypton/SplashSccreen.java
@@ -2,7 +2,12 @@ package com.example.crypton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class SplashSccreen extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class SplashSccreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_sccreen);
+
+        ImageView imageView = findViewById(R.id.imgLogo);
+
+        //inisiasi view
+        final ProgressBar progressBar = findViewById(R.id.progressSplash);
+        progressBar.setVisibility(View.VISIBLE);
+
+        setContentView(R.layout.activity_splash_sccreen);
+        //waktu splash screen
+        int splashscreen_time = 3500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //setelah waktu splashscreen habis, maka langsung berpindah ke Halaman Login
+                progressBar.setVisibility(View.GONE);
+                startActivity(new Intent(SplashSccreen.this, Home.class));
+                finish();
+            }
+        }, splashscreen_time);
+
     }
 }
