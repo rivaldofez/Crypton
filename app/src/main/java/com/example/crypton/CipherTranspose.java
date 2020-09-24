5ff78171f5118f6b94e19feb4b11c58d36abbbff
rivaldofez <rivaldofez@gmail.com>
2020-09-24 13:09:23 +0700

    make activity cipher transpose

---
+++ app/src/main/java/com/example/crypton/CipherTranspose.java
@@ -0,0 +1,14 @@
+package com.example.crypton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CipherTranspose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cipher_transpose);
+    }
+}
