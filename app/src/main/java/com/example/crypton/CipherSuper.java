b84cbcda3fe67ff5176d6116c8e526b14eec7adc
rivaldofez <rivaldofez@gmail.com>
2020-09-24 15:28:31 +0700

    make function cipher super

---
+++ app/src/main/java/com/example/crypton/CipherSuper.java
@@ -0,0 +1,14 @@
+package com.example.crypton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CipherSuper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cipher_super);
+    }
+}
