5bb4af76960001b92522d76de04760f2c5d32813
rivaldofez <rivaldofez@gmail.com>
2020-09-25 00:25:57 +0700

    fix frontend cipher text

--- app/src/main/java/com/example/crypton/Home.java
+++ app/src/main/java/com/example/crypton/Home.java
@@ -1,14 +1,47 @@
 package com.example.crypton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
+
+    ConstraintLayout containerCipher, containerTranspose, containerSuper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        containerCipher = findViewById(R.id.containerCipher);
+        containerSuper = findViewById(R.id.containerSuper);
+        containerTranspose = findViewById(R.id.containerTranspose);
+
+        containerCipher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, CipherText.class));
+                finish();
+            }
+        });
+
+        containerTranspose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, CipherTranspose.class));
+                finish();
+            }
+        });
+
+        containerSuper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, CipherSuper.class));
+                finish();
+            }
+        });
     }
 }
